a3ef617b6effdec37b0d7859ebdb4b85dbacfdd4
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2022-11-20 22:24:07 +0700

    login with context

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import {
   Box,
@@ -20,19 +20,53 @@ import {
 } from "@chakra-ui/react";
 
 import { BiIdCard, BiLockAlt, BiShow, BiHide } from "react-icons/bi";
+import { useRouter } from "next/router";
+import { AuthContext } from "../utils/AuthContext";
+import backend from "../api/backend";
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false);
   const [nim, setNim] = useState("");
   const [password, setPassword] = useState("");
+  const { setToken } = useContext(AuthContext);
+
+  const router = useRouter();
 
   const handleShowPassword = () => setShowPassword(!showPassword);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("nim: ", nim);
-    console.log("password: ", password);
+
+    if (nim === "" || password === "") {
+      alert("NIM dan Password tidak boleh kosong");
+      return;
+    }
+
+    const user = {
+      nim,
+      password,
+    }
+
+    handleLogin(user);
   };
+
+  const handleLogin = async (user) => {
+    try {
+      const res = await backend.post('auth/login', user, {
+        validateStatus: false,
+      });
+
+      if (res.status !== 200) {
+        alert(res.data.message);
+        return;
+      }
+
+      setToken(res.data.token);
+      router.push('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
     <Flex
